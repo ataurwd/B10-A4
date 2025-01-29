912b2ca05dd7f0bfbd19bb8c18ae6bc04b9748f0
Muhammad Ranju <mdranju23@gmail.com>
2025-01-30 01:16:31 +0600

    function for improved readability

--- p.js
+++ p.js
@@ -1,13 +1,15 @@
 function checkDigitsInName(name) {
-    if(typeof name !== 'string'){
-        return "Invalid Input"
+  if (typeof name !== "string") {
+    return "Invalid Input";
+  }
+  for (let i = 0; i < name.length; i++) {
+    if (!isNaN(name[i])) {
+      return true;
     }
-    for(let i = 0; i < name.length; i++){
-        if(!isNaN(name[i])){
-            return true;
-        }
-    }
-    return false;
+  }
+  return false;
 }
 
-console.log(checkDigitsInName(('Suman')));
+console.log(checkDigitsInName("Suman"));
+console.log(checkDigitsInName("Suman123"));
+console.log(checkDigitsInName("Suman1234567890"));
